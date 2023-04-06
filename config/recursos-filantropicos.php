710b0ac3432c1c527baf84a2fb31b8b86feec969
Luis Marin <marin084+github@gmail.com>
2023-04-06 12:14:09 -0600

    Directus Install

---
+++ config/recursos-filantropicos.php
@@ -0,0 +1,117 @@
+<?php
+
+return [
+    'database' => [
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'port' => 3306,
+        'name' => 'bo_recursosfilantropicos',
+        'username' => 'root',
+        'password' => 'q1w2e3r4t5y6',
+        'engine' => 'InnoDB',
+        'charset' => 'utf8mb4',
+        // Remove 'host' above when using sockets
+        'socket' => '',
+    ],
+
+    'cookie' => [
+        'same_site' => 'Lax',
+        'secure' => false
+    ],
+
+    'cors' => [
+        'enabled' => true,
+        'origin' => array (
+  0 => '*',
+),
+        'methods' => array (
+  0 => 'GET',
+  1 => 'POST',
+  2 => 'PUT',
+  3 => 'PATCH',
+  4 => 'DELETE',
+  5 => 'HEAD',
+),
+        'headers' => array (
+),
+        'exposed_headers' => array (
+),
+        'max_age' => 600,
+        'credentials' => true,
+    ],
+
+    'rate_limit' => [
+        'enabled' => false,
+        'limit' => 100,
+        'interval' => 60,
+        'adapter' => 'redis',
+        'host' => '127.0.0.1',
+        'port' => 6379,
+        'timeout' => 10,
+    ],
+
+    'storage' => [
+        'adapter' => 'local',
+        'root' => 'public/uploads/recursos-filantropicos/originals',
+        'root_url' => '/uploads/recursos-filantropicos/originals',
+        'thumb_root' => 'public/uploads/recursos-filantropicos/generated',
+        // 'key' => '',
+        // 'secret' => '',
+        // 'region' => '',
+        // 'version' => '',
+        // 'bucket' => '',
+        // 'options' => '',
+        // 'endpoint' => '',
+        // 'proxy_downloads' => '',
+    ],
+
+    'mail' => [
+        'default' => [
+            'transport' => 'sendmail',
+            // 'sendmail' => '',
+            // 'host' => '',
+            // 'port' => '',
+            // 'username' => '',
+            // 'password' => '',
+            // 'encryption' => '',
+            'from' => 'admin@example.com'
+        ],
+    ],
+
+    'cache' => [
+        'enabled' => false,
+        'response_ttl' => 3600,
+        'pool' => [
+            // 'adapter' => '',
+            // 'path' => '',
+            // 'host' => '',
+            // 'port' => '',
+        ],
+    ],
+
+    'auth' => [
+        'secret_key' => 'M9xQd9DxrVMBQSK2HQZRMHzzh07ITG3B',
+        'public_key' => '332a7165-4b63-43b5-a524-6032198279cc',
+        'ttl' => 20,
+        'social_providers' => [
+            // 'okta' => '',
+            // 'github' => '',
+            // 'facebook' => '',
+            // 'google' => '',
+            // 'twitter' => '',
+        ],
+    ],
+
+    'hooks' => [
+        'actions' => [],
+        'filters' => [],
+    ],
+
+    'tableBlacklist' => [],
+
+    'env' => 'production',
+
+    'logger' => [
+        'path' => '/Users/marin084/Sites/recursosfilantropicoscom/src/core/Directus/Util/Installation/../../../../../logs',
+    ],
+];
